aeae11153d5d43edf7cb83300b51197b5d5e42ff
HawtStrokes <hawtstrokes1337@gmail.com>
2021-02-11 09:03:37 -0800

    added text formatting library to HawtText namespace

--- HawtLib/Source/HawtText/TextFormatting.cpp
+++ HawtLib/Source/HawtText/TextFormatting.cpp
@@ -3,62 +3,67 @@
 
 #include "TextFormatting.h"
 
-std::string CenterText(const std::string& inStr, size_t space) {
-	if (space < inStr.size()) {
-		printf("space must be atleast euqal to str size");
-		__debugbreak();
-	}
-	return std::string((space - inStr.size()) / 2, ' ') + inStr + std::string((space - inStr.size()) / 2, ' ');
-}
+namespace HawtLib {
+	namespace HawtText {
 
-std::string ToLower(const std::string& inStr) {
-	std::string outStr = "";
-	for (char c : inStr) {
-		if (c <= 90 && c >= 65) c += 32;
-		outStr += c;
-	}
-	return outStr;
-}
+		std::string CenterText(const std::string& inStr, size_t space) {
+			if (space < inStr.size()) {
+				printf("space must be atleast euqal to str size");
+				__debugbreak();
+			}
+			return std::string((space - inStr.size()) / 2, ' ') + inStr + std::string((space - inStr.size()) / 2, ' ');
+		}
 
-char ToLower( char inC) {
-	if (inC <= 90 && inC >= 65) return inC += 32;
-}
+		std::string ToLower(const std::string& inStr) {
+			std::string outStr = "";
+			for (char c : inStr) {
+				if (c <= 90 && c >= 65) c += 32;
+				outStr += c;
+			}
+			return outStr;
+		}
 
-std::string ToUpper(const std::string& inStr) {
-	std::string outStr;
-	for (char c : inStr) {
-		if (c <= 122 && c >= 97) c -= 32;
-		outStr += c;
-	}
-	return outStr;
-}
+		char ToLower(char inC) {
+			if (inC <= 90 && inC >= 65) return inC += 32;
+		}
 
-char ToUpper(char inC) {
-	if (inC <= 122 && inC >= 97) return inC -= 32;
-}
+		std::string ToUpper(const std::string& inStr) {
+			std::string outStr;
+			for (char c : inStr) {
+				if (c <= 122 && c >= 97) c -= 32;
+				outStr += c;
+			}
+			return outStr;
+		}
 
-std::string Title(const std::string& inStr) {
-	std::string outStr = "";
-	std::vector<std::string> words = SplitString(inStr);
-	for (auto& word : words) {
-		word[0] = ToUpper(word[0]);
-		outStr += word;
-	}
-	return outStr;
-}
+		char ToUpper(char inC) {
+			if (inC <= 122 && inC >= 97) return inC -= 32;
+		}
 
-std::vector<std::string> SplitString(const std::string& inStr, char delim) {
-	std::vector<std::string> outVec;
-	std::string word = "";
-	for (char c : inStr) {
-		if (c != delim) word += c;
-		else {
-			outVec.emplace_back(word);
-			word = "";
+		std::string Title(const std::string& inStr) {
+			std::string outStr = "";
+			std::vector<std::string> words = SplitString(inStr);
+			for (auto& word : words) {
+				word[0] = ToUpper(word[0]);
+				outStr += word;
+			}
+			return outStr;
+		}
+
+		std::vector<std::string> SplitString(const std::string& inStr, char delim) {
+			std::vector<std::string> outVec;
+			std::string word = "";
+			for (char c : inStr) {
+				if (c != delim) word += c;
+				else {
+					outVec.emplace_back(word);
+					word = "";
+				}
+			}
+			if (!word.empty()) {
+				outVec.emplace_back(word);
+			}
+			return outVec;
 		}
 	}
-	if (!word.empty()) {
-		outVec.emplace_back(word);
-	}
-	return outVec;
 }
--- HawtLib/Source/HawtText/TextFormatting.h
+++ HawtLib/Source/HawtText/TextFormatting.h
@@ -5,10 +5,15 @@ namespace std {
 	using string = basic_string<char, char_traits<char>, allocator<char>>;
 }
 
-std::string CenterText(const std::string& inStr, size_t space = 120);
-std::string ToLower(const std::string& inStr);
-char ToLower(char inC);
-std::string ToUpper(const std::string& inStr);
-char ToUpper(char inC);
-std::string Title(const std::string& inStr);
-std::vector<std::string> SplitString(const std::string& inStr, char delim = ' ');
+namespace HawtLib {
+
+	namespace HawtText {
+		std::string CenterText(const std::string& inStr, size_t space = 120);
+		std::string ToLower(const std::string& inStr);
+		char ToLower(char inC);
+		std::string ToUpper(const std::string& inStr);
+		char ToUpper(char inC);
+		std::string Title(const std::string& inStr);
+		std::vector<std::string> SplitString(const std::string& inStr, char delim = ' ');
+	}
+}
