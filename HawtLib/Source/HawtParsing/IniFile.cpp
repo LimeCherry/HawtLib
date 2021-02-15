3a79fff44c2d8a2f3b832ebacf9f98327468b4e9
HawtStrokes <hawtstrokes1337@gmail.com>
2021-02-15 05:26:31 -0800

    Fixed the copy assignment operator

--- HawtLib/Source/HawtParsing/IniFile.cpp
+++ HawtLib/Source/HawtParsing/IniFile.cpp
@@ -25,17 +25,18 @@ namespace HawtLib {
 		}
 
 		IniFile& IniFile::operator=(IniFile& other) {
-			other.m_Sections.reserve(m_Sections.size());
-			for (Section* m_Section : m_Sections) {
-				Section* section = new Section{ m_Section->name };
+			m_Sections.reserve(other.m_Sections.size());
+			for (Section* otherSection : other.m_Sections) {
+				Section* section = new Section{ otherSection->name };
 				std::vector<KeyValue*> keyValues{};
-				keyValues.reserve(m_Section->keyValues.size());
-				for (size_t i = 0; i < m_Section->keyValues.size(); ++i) {
-					keyValues.emplace_back(new KeyValue{ m_Section->keyValues[i]->key,
-						m_Section->keyValues[i]->value });
+				keyValues.reserve(otherSection->keyValues.size());
+				for (size_t i = 0; i < otherSection->keyValues.size(); ++i) {
+					keyValues.emplace_back(new KeyValue{ otherSection->keyValues[i]->key,
+						otherSection->keyValues[i]->value });
 				}
-				other.m_Sections.emplace_back(section);
+				m_Sections.emplace_back(section);
 			}
+			return *this;
 		}
 
 		IniFile::~IniFile() {
