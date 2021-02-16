0d7859f2fdf73362272037ae1f7e3c80d1b7a6e3
HawtStrokes <hawtstrokes1337@gmail.com>
2021-02-15 18:43:52 -0800

    wrapped definitions in namespace regions instead of using the scope resolution operator

--- HawtLib/Source/HawtCrypto/Hashing.cpp
+++ HawtLib/Source/HawtCrypto/Hashing.cpp
@@ -1,17 +1,22 @@
 #include "Hashing.h"
 
-// modernization of the hashing algorithm djb2
-unsigned long long HawtLib::HawtCrypto::DJB2(const std::string& str) {
-    unsigned long long hashValue = 5381;
-    for (char c : str) {
-        hashValue = ((hashValue << 5) + hashValue) + c;
+namespace HawtLib {
+    namespace HawtCrypto {
+        unsigned long long DJB2(const std::string& str) {
+            unsigned long long hashValue = 5381;
+            for (char c : str) {
+                hashValue = ((hashValue << 5) + hashValue) + c;
+            }
+            return hashValue;
+        }
+
+        unsigned long long SDBM(const std::string& str) {
+            unsigned long long hashValue = 0;
+            for (char c : str)
+                hashValue = c + (hashValue << 6) + (hashValue << 16) - hashValue;
+            return hashValue;
+        }
+
     }
-    return hashValue;
 }
 
-unsigned long long HawtLib::HawtCrypto::SDBM(const std::string& str) {
-    unsigned long long hashValue = 0;
-    for (char c : str) 
-        hashValue = c + (hashValue << 6) + (hashValue << 16) - hashValue;
-    return hashValue;
-}
